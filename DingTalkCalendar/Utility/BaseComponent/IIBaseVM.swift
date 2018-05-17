6d33ead53dc1ac18b9cb54071b7b11b3a334e59d
Noah_Shan <shanwzh@inspur.com>
2018-05-17 10:17:29 +0800

    = 添加base vm =

---
+++ DingTalkCalendar/Utility/BaseComponent/IIBaseVM.swift
@@ -0,0 +1,17 @@
+//
+//  IIBaseVM.swift
+//  DingTalkCalendar
+//
+//  Created by Noah_Shan on 2018/5/16.
+//  Copyright © 2018年 Inspur. All rights reserved.
+//
+
+import Foundation
+
+class IIBaseVM: NSObject {
+    
+    override init() {
+        super.init()
+    }
+    
+}
